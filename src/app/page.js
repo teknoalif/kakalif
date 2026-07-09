e77088dc74ccb7aa2eafe2cfb90ea537ec8f8347
Alif Rezky <teknoalif@mailo.com>
2026-07-09 23:00:23 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-import Hero from '@/components/Hero';
-import Products from '@/components/Products';
-import Features from '@/components/Features';
-import Testimonials from '@/components/Testimonials';
-import Footer from '@/components/Footer';
+import Hero from '../components/Hero';
+import Products from '../components/Products';
+import Features from '../components/Features';
+import Testimonials from '../components/Testimonials';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
