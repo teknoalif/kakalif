19f31d8aec3b8808686fe787a56f5d84b435055b
Alif Rezky <teknoalif@mailo.com>
2026-07-10 00:05:23 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
-import Navbar from '../components/Navbar'; // <-- Tambahkan ini
+import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Products from '../components/Products';
 import Features from '../components/Features';
 import Portfolio from '../components/Portfolio';
+import Achievements from '../components/Achievements';
 import Testimonials from '../components/Testimonials';
 import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <main className="min-h-screen pt-16"> {/* Ditambahkan pt-16 agar konten Hero tidak tertutup Navbar */}
-      <Navbar /> {/* Sekaligus ditaruh di sini */}
+    <main className="min-h-screen pt-16">
+      <Navbar />
       <Hero />
+      <Achievements />
       <Portfolio />
       <Products />
       <Features />
