a658fa42754fa57d942d79efea05a2b4483eaf1e
Alif Rezky <teknoalif@mailo.com>
2026-07-15 00:33:20 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Navbar from '../components/Navbar';
+import BookPromo from '../components/BookPromo'; // <-- Tambahkan ini
 import Hero from '../components/Hero';
 import Products from '../components/Products';
 import Features from '../components/Features';
@@ -11,11 +12,12 @@ export default function Home() {
   return (
     <main className="min-h-screen pt-16">
       <Navbar />
+      <BookPromo /> {/* <-- Dipasang tepat sebelum HERO */}
       <Hero />
-      <Achievements />
-      <Portfolio />
       <Products />
       <Features />
+      <Portfolio />
+      <Achievements />
       <Testimonials />
       <Footer />
     </main>
